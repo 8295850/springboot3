fb6707655996f8611e12de14c2369e70a363d7b3
8295850 <79565022+8295850@users.noreply.github.com>
2022-07-11 05:26:39 +0800

    111
    111

--- src/main/java/com/study/test.java
+++ src/main/java/com/study/test.java
@@ -3,5 +3,7 @@ package com.study;
 public class test {
     public static void main(String[] args) {
         System.out.println("123123");
+         System.out.println("123123");
+         System.out.println("123123");
     }
 }
